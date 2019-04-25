e12d67557f8af141dbed7a5f20031d9e2769e83e
Anastasia Afanasenko <37610506+Nastiaaf@users.noreply.github.com>
2019-04-25 22:15:57 +0300

    Add files via upload

---
+++ java/com/example/nastia/noteskeeper/NotesBuilder.java
@@ -0,0 +1,21 @@
+package com.example.nastia.noteskeeper;
+
+public class NotesBuilder {
+    private String title, content;
+
+    public NotesBuilder() {
+    }
+
+    public NotesBuilder(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
